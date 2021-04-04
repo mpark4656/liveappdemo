9b32061d796301dc617ce08a8edde53fbc1a4a2d
mpark4656 <mpark4656@gmail.com>
2021-04-04 13:36:06 -0400

    Finished Javascript number

---
+++ js/js/numbers.js
@@ -0,0 +1,8 @@
+console.log(2 + 4 * 6 + 1);
+if(6 % 2 === 0) console.log('6 is even.');
+if(5 % 2 === 1) console.log('5 is odd.');
+console.log('2 to the 6th power is ' + 2 ** 6);
+console.log('0 divided by 0 is ' + 0 / 0);
+console.log('1 divided by 0 is ' + 1 / 0);
+console.log('Type of NaN is ' + typeof NaN);
+console.log('Type of Infinity is ' + typeof Infinity);
