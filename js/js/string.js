28a27c2d2ce8111189135323a9e9c0ef4088c15c
mpark4656 <mpark4656@gmail.com>
2021-05-01 23:15:08 -0400

    Fixes

---
+++ js/js/string.js
@@ -0,0 +1,7 @@
+let doubleQuoteStr = "String with double quotation";
+let singleQuoteStr = 'String with single quotation';
+let backTickStr = `String with backtick marks or template string ${5 + 2}`;
+
+console.log(doubleQuoteStr);
+console.log(singleQuoteStr);
+console.log(backTickStr);
