819c7d6bf96561056beeb5e1a0f4da54402f26d4
mpark4656 <mpark4656@gmail.com>
2021-05-11 23:43:29 -0400

    Added Math object

---
+++ js/js/math.js
@@ -0,0 +1,7 @@
+console.log(`The value of PI is ${Math.PI}`);
+console.log(`Floor always rounds down - ${Math.floor(2.67)}`);
+console.log(`Ceiling always rounds up - ${Math.ceil(2.67)}`);
+console.log(`The square root of 4 is ${Math.sqrt(4)}`);
+
+let randomInteger = Math.floor(Math.random() * 10);
+console.log(randomInteger);
