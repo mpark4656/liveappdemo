7962ddcd1ccbea59ce1277e3344a57ce0e9e1aab
mpark4656 <mpark4656@gmail.com>
2021-05-14 21:59:38 -0400

    Double vs Triple equality

--- js/js/comparison-operators.js
+++ js/js/comparison-operators.js
@@ -5,3 +5,16 @@
 1 < 1   // false
 
 "test" == "test"   // true
+
+1 == 1              // true
+1 == '1'            // true
+null == undefined   // true
+0 == false          // true
+
+1 === 1              // true
+1 === '1'            // false
+null === undefined   // false
+0 === false          // false
+
+0 !== '0'   // true
+0 != '0'    // false
