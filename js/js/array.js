db455164e8ea7c7e7d4b75e700386c0c0ffa60f6
Michael Park <mpark4656@gmail.com>
2021-06-27 15:38:09 -0400

    Worked on javascript array

---
+++ js/js/array.js
@@ -0,0 +1,28 @@
+let movieLine = ['tom', 'nancy', 'pablo', 'oliver', 'eva'];
+
+// Show the current array
+console.log(movieLine);
+
+// Remove the first element
+movieLine.shift();
+
+// Show the current array
+console.log(movieLine);
+
+// Add jerry to the beginning of the array
+movieLine.unshift('jerry');
+
+// Show the current array
+console.log(movieLine);
+
+// Remove the last element
+movieLine.pop();
+
+// Show the current array
+console.log(movieLine);
+
+// Add mike to the end of the array
+movieLine.push('mike');
+
+// Show the current array
+console.log(movieLine);
